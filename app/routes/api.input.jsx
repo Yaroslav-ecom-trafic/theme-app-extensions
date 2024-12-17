ee6ff30a967042e695e27b9593f3353e7827b68d
Yaroslav-Vasin <yaroslav@ecomtraffic.nl>
2024-12-17 21:52:24 +0200

    done

--- app/routes/api.input.jsx
+++ app/routes/api.input.jsx
@@ -3,3 +3,18 @@ import { json } from "@remix-run/node";
 export async function loader() {
     return json({ text: "привет" });
 }
+
+export async function action({ request }) {
+
+    console.log("request", request);
+    if (request.method === "POST") {
+        const payload = await request.json();
+
+        return json({ text: `перевернутая строка: ${stringReverse(payload.text)}` });
+    }
+    return json({ error: "Метод не поддерживается" }, { status: 405 });
+}
+
+function stringReverse(str) {
+    return str.split('').reverse().join('');
+}
--- extensions/input-extension/assets/input-app.js
+++ extensions/input-extension/assets/input-app.js
@@ -1,19 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('.input-app-form');
+    const inputAppResult = document.querySelector('.input-app-result');
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
 
+        const input = form.querySelector('.input-app-input');
+
         try {
-            const response = await fetch('https://rouge-outdoors-cd-liz.trycloudflare.com/api/input', {
+            const response = await fetch('https://survival-islands-mineral-icon.trycloudflare.com/api/input', {
                 method: 'GET',
                 headers: {
                     'Accept': 'application/json'
                 }
             });
 
+            const data1 = await response.json();
+            console.log('Ответ:', data1.text);
+
+        } catch (error) {
+            console.log('Ошибка:', error);
+        }
+
+        try {
+            // Текст, который будет отправлен на сервер
+            const payload = { text: input.value };
+
+            // POST-запрос
+            const response = await fetch('https://survival-islands-mineral-icon.trycloudflare.com/api/input', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json', // Указываем тип контента
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(payload) // Отправляем JSON
+            });
+
             const data = await response.json();
-            console.log('Ответ:', data.text);
+
+            inputAppResult.insertAdjacentHTML('beforeend', `<p>${data.text}</p>`);
 
         } catch (error) {
             console.log('Ошибка:', error);
