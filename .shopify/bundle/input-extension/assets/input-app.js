382e2f318149c7faf0db12a15dbcd300c449e346
Yaroslav-Vasin <yaroslav@ecomtraffic.nl>
2024-12-17 21:06:20 +0200

    response

--- .shopify/bundle/input-extension/assets/input-app.js
+++ .shopify/bundle/input-extension/assets/input-app.js
@@ -1,29 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('.input-app-form');
-    const resultElement = document.querySelector('.input-app-result p');
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
 
-        const response = await fetch('app/input', {
-            method: 'POST',
-            body: JSON.stringify({ username: "example" })
-        });
+        try {
+            const response = await fetch('https://rouge-outdoors-cd-liz.trycloudflare.com/api/input', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
 
-        // const input = form.querySelector('.input-app-input');
-        // const formData = new FormData();
-        // formData.append('input', input.value);
+            const data = await response.json();
+            console.log('Ответ:', data.text);
 
-        // try {
-        //     const response = await fetch('app/input', {
-        //         method: 'POST',
-        //         body: JSON.stringify({ username: "example" })
-        //     });
-
-        //     const data = await response.json();
-        //     resultElement.textContent = data.result;
-        // } catch (error) {
-        //     resultElement.textContent = 'Error: ' + error.message;
-        // }
+        } catch (error) {
+            console.log('Ошибка:', error);
+        }
     });
 });
